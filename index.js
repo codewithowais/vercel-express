392450709af4b2dfe4a5d81ca486ca846b1b3138
codewithowais <codewithowais@gmail.com>
2024-01-21 14:50:05 +0500

    add some more routes

--- index.js
+++ index.js
@@ -5,11 +5,18 @@ app.get("/", (req, res) => {
   res.send("Express on Vercel");
 });
 
+app.get("/home", (req, res) => {
+  res.send("HEllo.. this is home page");
+});
+
+app.get("/about", (req, res) => {
+  res.send("HEllo.. this is home about");
+});
+
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
-
 
 module.exports = app;
