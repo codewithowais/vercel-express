59089bd7a651dfad7238904db10b4b8514e8973c
codewithowais <codewithowais@gmail.com>
2024-01-21 14:47:44 +0500

    vercel nodejs deploy

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Express on Vercel");
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
+
+module.exports = app;
